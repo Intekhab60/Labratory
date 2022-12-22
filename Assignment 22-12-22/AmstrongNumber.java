db1feb6d1634e97c7a81269bcc740a2bf0ff56a5
Intekhab60 <120620849+Intekhab60@users.noreply.github.com>
2022-12-22 19:33:50 +0530

    Create AmstrongNumber.java

---
+++ Assignment 22-12-22/AmstrongNumber.java
@@ -0,0 +1,30 @@
+//WAP to check wheather a number is a amstrong number or not.
+
+ package basic;
+
+import java.util.Scanner;
+
+public class AmstrongNumber 
+{
+	public static void main(String[] args)
+	{
+		Scanner Sc = new Scanner(System.in);
+		int num,rem,sum=0,temp;  		
+		System.out.println("Enter the number:");    
+		num =Sc.nextInt();
+		int actual = num;
+		   
+		temp=num;    
+		while(num>0)    
+		{    
+			rem=num%10;    
+			sum=sum+(rem*rem*rem);    
+			num=num/10;
+		}    
+		if(temp==sum)    
+		System.out.println(actual+" is a armstrong  number.");    
+		else    
+		System.out.println(actual+" is not a armstrong number."); 
+	}
+
+} 
