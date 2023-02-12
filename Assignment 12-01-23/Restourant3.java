5b4bd3918472bc35e3c4e44b3f7b51e1d60da5e3
Intekhab60 <120620849+Intekhab60@users.noreply.github.com>
2023-02-12 16:29:04 +0530

    Create Restourant3.java

---
+++ Assignment 12-01-23/Restourant3.java
@@ -0,0 +1,26 @@
+package restourantmanagement;
+
+public class Restourant3 {
+	String fname;
+	int count;
+	double fprice;
+	public String getFname() {
+		return fname;
+	}
+	public int getCount() {
+		return count;
+	}
+	public double getFprice() {
+		return fprice;
+	}
+	public Restourant3(String fname, int count, double fprice) {
+		super();
+		this.fname = fname;
+		this.count = count;
+		this.fprice = fprice;
+	}
+	
+}
+
+
+
