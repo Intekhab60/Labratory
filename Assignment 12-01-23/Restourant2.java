a35c99a7db48b7177f93e979b75ab7056957d85c
Intekhab60 <120620849+Intekhab60@users.noreply.github.com>
2023-02-12 16:31:43 +0530

    Create Restourant2.java

---
+++ Assignment 12-01-23/Restourant2.java
@@ -0,0 +1,12 @@
+
+package restourantmanagement;
+//class to call methods
+public class Restourant2 {
+
+	public static void main(String[] args) {
+		Restourant1 ob=new Restourant1();
+		ob.menu();//call method menu
+		ob.order();//call method order
+	}
+
+}
